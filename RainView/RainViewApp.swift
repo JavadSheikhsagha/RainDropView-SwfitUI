3a9a800da8421e60760b6a17a5a7309e6ba3343c
Javad <sir.hxt@gmail.com>
2023-07-10 13:04:09 +0330

    Initial Commit

---
+++ RainView/RainViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainViewApp.swift
+//  RainView
+//
+//  Created by enigma 1 on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RainViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
